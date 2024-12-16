7bc7db7bdf43e5a2bbd908b6b5e86ff80b110ab9
master <bryngowi2@gmail.com>
2024-12-16 09:32:09 +0300

    v1

--- app/src/main/java/com/example/CiveServices/MainActivity.java
+++ app/src/main/java/com/example/CiveServices/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.CiveServices;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +15,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button start, stop;
+    private static final int pic_id = 123;
+    Button camera_open_id;
+    ImageView click_image_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        camera_open_id = findViewById(R.id.camera_button);
+        click_image_id = findViewById(R.id.click_image);
         start = findViewById(R.id.startbtn);
         stop = findViewById(R.id.stopbtn);
 
@@ -30,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, MyService.class);
             stopService(intent);
         });
+        camera_open_id.setOnClickListener(v -> {
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent, pic_id);
+        });
+        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == pic_id) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            click_image_id.setImageBitmap(photo);
+        }
         }
 
     }
